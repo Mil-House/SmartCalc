34e40226901fbd92f7d4c9fd2d91aac886aa4590
MILHOUSE <87607221+Mil-House@users.noreply.github.com>
2023-11-23 23:12:57 +0400

    Update calculator-AMD.js

--- assets/js/calculator-AMD.js
+++ assets/js/calculator-AMD.js
@@ -240,13 +240,17 @@ document.getElementById("usdRate").value = usdRate || '400';
 
 // Обработчик изменения курса USD
 document.getElementById("usdRate").addEventListener('input', function() {
+  const inputValue = this.value.trim();
 
-  // Установить значение usdRate введенным значением
-  usdRate = parseFloat(inputValue) || null;
-  displayItems();
-  calculateTotalCost(); // При изменении курса вызываем пересчет общей стоимости
+  if (inputValue.length >= 3 && inputValue <= 550 && !isNaN(parseFloat(inputValue))) {
+    // Установить значение usdRate введенным значением
+    usdRate = parseFloat(inputValue);
+    displayItems();
+    calculateTotalCost(); // При изменении курса вызываем пересчет общей стоимости
+  }
 });
 
+// Обработчик изменения курса USD (проверка при завершении ввода)
 document.getElementById("usdRate").addEventListener('change', function() {
   const inputValue = this.value.trim();
 
@@ -254,6 +258,11 @@ document.getElementById("usdRate").addEventListener('change', function() {
     // Показать предупреждение, если введено менее трех цифр или значение не является числом
     alert("Пожалуйста, введите корректный курс USD, содержащий не менее трех цифр.");
     this.value = "400"; // By Default 400
+  } else {
+    // Установить значение usdRate введенным значением
+    usdRate = parseFloat(inputValue);
+    displayItems();
+    calculateTotalCost(); // При изменении курса вызываем пересчет общей стоимости
   }
 });
 
